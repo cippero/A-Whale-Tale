d611cbc1f46604ae110fae12a157c21a2ae1d9b7
cippero <gilwein@gmail.com>
2018-04-07 16:10:54 -0700

    wrestling with phaser

--- code/game.js
+++ code/game.js
@@ -1,32 +1,38 @@
 let game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update });
 
 function preload() {
-
     game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-    game.load.image("bomb", "assets/bomb.png");
-
+    game.load.image('ground', 'assets/platform.png');
+    // game.load.image('star', 'assets/star.png');
+    // game.load.image("bomb", "assets/bomb.png");
+        // game.load.tilemap("map", "assets/tiles.json", null, Phaser.Tilemap.TILED_JSON);
+        // game.load.image("level", "assets/tiles.png")
 }
 
 let player;
-let platforms;
 let cursors;
-
-let stars;
 let score = 0;
+let platforms;
+let stars;
 let scoreText;
+let skyBackground;
+    // let map;
+    // let layer;
 
-// let speed = 15;
-// let speedAcceleratorX = speed;
-// let speedAcceleratorY = speed;
-
-
-function create() {
+function create() { 
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.sprite(0, 0, 'sky');
+
+    game.stage.backgroundColor = '#787878';
+
+        // map = game.add.tilemap("map");
+        // map.addTilesetImage("level");
+        // map.setCollisionBetween(1,9);
+        // layer = map.createLayer("Tile Layer 1");
+        // layer.resizeWorld();
+
+    skyBackground = game.add.tileSprite(0, 0, 800, 600, 'sky');
 
     platforms = game.add.group();
     platforms.enableBody = true;
@@ -50,116 +56,47 @@ function create() {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    game.camera.follow(player);
 
-    stars = game.add.group();
-    stars.enableBody = true;
+    // stars = game.add.group();
+    // stars.enableBody = true;
     //createStars(12);
     
+    // scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
-
-    
 }
+
 
 function update() {
 
-    //  Collide the player and the stars with the platforms
+    skyBackground.tilePosition.y += 2;
     let hitPlatform = game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
+    // game.physics.arcade.collide(stars, platforms);
+    // game.physics.arcade.collide(player, layer);
 
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+    // game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
-    if (cursors.left.isDown) {      //left = below zero
-        if (player.body.velocity.x > 0){
-            player.body.acceleration.x -= 10;
-        } else {
-            player.body.acceleration.x -= 1;
-        }
-        player.animations.play("left");
-
-    } else if (cursors.right.isDown) {      //right = above zero
-        if (player.body.velocity.x < 0){
-            player.body.acceleration.x += 10;
-        } else {
-            player.body.acceleration.x += 1;
-        }
-        player.animations.play("right");
-    } else {
-        player.animations.stop();
-        player.frame = 4;
-
-        if (player.body.velocity.x < -10){
-            player.body.acceleration.x += 5;
-        } else if (player.body.velocity.x > -10 && player.body.velocity.x < 0){
-            player.body.acceleration.x = 0;
-        } else if (player.body.velocity.x > 10){
-            player.body.acceleration.x -= 5;
-        } else if (player.body.velocity.x < 10 && player.body.velocity.x > 0){
-            player.body.acceleration.x = 0;
-        }
-    }
-
-
-    if (cursors.up.isDown) {      //left = below zero
-        if (player.body.velocity.y > 0){
-            player.body.acceleration.y -= 10;
-        } else {
-            player.body.acceleration.y -= 1;
-        }
-        player.animations.play("left");
-
-    } else if (cursors.down.isDown) {      //right = above zero
-        if (player.body.velocity.y < 0){
-            player.body.acceleration.y += 10;
-        } else {
-            player.body.acceleration.y += 1;
-        }
-        player.animations.play("right");
-    } else {
-        player.animations.stop();
-        player.frame = 4;
-
-        if (player.body.velocity.y < -10){
-            player.body.acceleration.y += 5;
-        } else if (player.body.velocity.y > -10 && player.body.velocity.y < 0){
-            player.body.acceleration.y = 0;
-        } else if (player.body.velocity.y > 10){
-            player.body.acceleration.y -= 5;
-        } else if (player.body.velocity.y < 10 && player.body.velocity.y > 0){
-            player.body.acceleration.y = 0;
-        }
-    }
-
+    playerControls();
 }
 
-function moveDirection(speed, axis, animation) {
-        if (axis === "x") {
-            player.body.velocity.x = speed;
-        } else {
-            player.body.velocity.y = speed;
-        }
-        player.animations.play(animation);
-}
 
-function collectStar (player, star) {
-    star.kill();
-    score += 10;
-    scoreText.text = 'Score: ' + score;
-    // if (stars.total === 6){
-    //     let x = (player.x < 400) ? game.rnd.integerInRange(400, 800) : game.rnd.integerInRange(0, 400); 
-    //     //assigns x value in right half of screen if player is in left half, otherwise in left half
+// function collectStar (player, star) {
+//     star.kill();
+//     score += 10;
+//     scoreText.text = 'Score: ' + score;
+//     // if (stars.total === 6){
+//     //     let x = (player.x < 400) ? game.rnd.integerInRange(400, 800) : game.rnd.integerInRange(0, 400); 
+//     //     //assigns x value in right half of screen if player is in left half, otherwise in left half
 
-}
+// }
 
-function createStars(num) {
-    for (let i = 0; i < num; i++) {
-        let star = stars.create(i * game.world.width/num+3.5*i, 0, 'star');
-        star.body.gravity.y = 300;
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
-}
+
+// function createStars(num) {
+//     for (let i = 0; i < num; i++) {
+//         let star = stars.create(i * game.world.width/num+3.5*i, 0, 'star');
+//         star.body.gravity.y = 300;
+//         star.body.bounce.y = 0.7 + Math.random() * 0.2;
+//     }
+// }
 
---
+++ code/player.js
@@ -0,0 +1,62 @@
+function playerControls() {    
+    if (cursors.left.isDown) {      //left = below zero
+        if (player.body.velocity.x > 0){
+            player.body.acceleration.x -= 10;
+        } else {
+            player.body.acceleration.x -= 1;
+        }
+        player.animations.play("left");
+
+    } else if (cursors.right.isDown) {      //right = above zero
+        if (player.body.velocity.x < 0){
+            player.body.acceleration.x += 10;
+        } else {
+            player.body.acceleration.x += 1;
+        }
+        player.animations.play("right");
+    } else {
+        player.animations.stop();
+        player.frame = 4;
+
+        if (player.body.velocity.x < -10){
+            player.body.acceleration.x += 5;
+        } else if (player.body.velocity.x > -10 && player.body.velocity.x < 0){
+            player.body.acceleration.x = 0;
+        } else if (player.body.velocity.x > 10){
+            player.body.acceleration.x -= 5;
+        } else if (player.body.velocity.x < 10 && player.body.velocity.x > 0){
+            player.body.acceleration.x = 0;
+        }
+    }
+
+
+    if (cursors.up.isDown) {      //left = below zero
+        if (player.body.velocity.y > 0){
+            player.body.acceleration.y -= 10;
+        } else {
+            player.body.acceleration.y -= 1;
+        }
+        player.animations.play("left");
+
+    } else if (cursors.down.isDown) {      //right = above zero
+        if (player.body.velocity.y < 0){
+            player.body.acceleration.y += 10;
+        } else {
+            player.body.acceleration.y += 1;
+        }
+        player.animations.play("right");
+    } else {
+        player.animations.stop();
+        player.frame = 4;
+
+        if (player.body.velocity.y < -10){
+            player.body.acceleration.y += 5;
+        } else if (player.body.velocity.y > -10 && player.body.velocity.y < 0){
+            player.body.acceleration.y = 0;
+        } else if (player.body.velocity.y > 10){
+            player.body.acceleration.y -= 5;
+        } else if (player.body.velocity.y < 10 && player.body.velocity.y > 0){
+            player.body.acceleration.y = 0;
+        }
+    }
+}
